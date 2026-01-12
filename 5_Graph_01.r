f3f746a19a20c30a7e58bfd80d794fb8ba80fa57
JBC <joci1358@student.su.se>
2026-01-13 00:45:55 +0100

    Another test

--- 5_Graph_01.r
+++ 5_Graph_01.r
@@ -35,18 +35,31 @@ data_xts <- xts(
 
 print(dim(data_xts))
 
-png("Processed_Data/Graph_01_Plotted_Processed_Variables.png", width = 1000, height = 1000)
+png("Processed_Data/Graph_01_Plotted_Processed_Variables.png", width = 1200, height = 1000)
 par(mfrow = c(2, 2))
 {
-  plot(data_xts$real_oil_price, main = "Real Oil Price (Log levels)", ylab = "Log Price", col = "darkgreen", lwd = 1)
-  plot(data_xts$oil_production_growth, main = "Global Oil Production Growth (%)", ylab = "Percent Change", col = "brown", lwd = 1)
-  plot(data_xts$real_activity, main = "Global Real Economic Activity", ylab = "Index", col = "blue", lwd = 1)
-  plot(data_xts$real_sp500_return, main = "Real S&P 500 Monthly Returns (%)", ylab = "Real Return (%)", col = "purple", lwd = 1)
+  plot(data_xts$real_oil_price, 
+       main = "Real Oil Price (Log levels)",
+       ylab = "Log Price",
+       col = "darkgreen",
+       lwd = 1)
+  
+  plot(data_xts$oil_production_growth, 
+       main = "Global Oil Production Growth (%)",
+       ylab = "Percent Change",
+       col = "brown",
+       lwd = 1)
+  
+  plot(data_xts$real_activity, 
+       main = "Global Real Economic Activity",
+       ylab = "Index",
+       col = "blue",
+       lwd = 1)
+  
+  plot(data_xts$real_sp500_return, 
+       main = "Real S&P 500 Monthly Returns (%)",
+       ylab = "Real Return (%)",
+       col = "purple",
+       lwd = 1)
 }
-dev.off()
-
-#source("plot_graphs.R", echo = TRUE)
-
-png("test.png")
-plot(1:10)
 dev.off()
